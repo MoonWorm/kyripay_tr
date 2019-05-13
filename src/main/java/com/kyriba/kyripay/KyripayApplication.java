6d73d4fccaa2b7cfed5a52e527e0b1bedce668ce
Alexey Stankevich <astankevich@kyriba.com>
2019-05-13 20:45:51 +0300

    Init converter service

---
+++ src/main/java/com/kyriba/kyripay/KyripayApplication.java
@@ -0,0 +1,14 @@
+package com.kyriba.kyripay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyripayApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyripayApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kyriba/kyripay/converter/Document.java
@@ -0,0 +1,10 @@
+package com.kyriba.kyripay.converter;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Document {
+    private String data;
+}
