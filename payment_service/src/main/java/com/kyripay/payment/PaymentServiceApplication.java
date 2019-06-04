1f629cd5ec71ebade06a332e46860b937317d675
Alexey Talyuk <ataliuk@kyriba.com>
2019-06-04 23:34:51 +0300

    add service discovery support, add simple Dockerfile, add sql migrations support

--- payment_service/src/main/java/com/kyripay/payment/PaymentServiceApplication.java
+++ payment_service/src/main/java/com/kyripay/payment/PaymentServiceApplication.java
@@ -2,7 +2,10 @@ package com.kyripay.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 public class PaymentServiceApplication
 {
