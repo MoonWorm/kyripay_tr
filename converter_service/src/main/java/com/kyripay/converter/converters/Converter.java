9129a11e966012bbfdec3cb4c26f53b1bb0e759b
Alexey Stankevich <astankevich@kyriba.com>
2019-06-26 15:04:14 +0300

    Address comments

--- converter_service/src/main/java/com/kyripay/converter/converters/Converter.java
+++ converter_service/src/main/java/com/kyripay/converter/converters/Converter.java
@@ -2,10 +2,8 @@ package com.kyripay.converter.converters;
 
 import com.kyripay.converter.dto.FormatDetails;
 import com.kyripay.converter.dto.Payment;
-import org.springframework.stereotype.Component;
 
 
-@Component
 public interface Converter
 {
   FormatDetails getFormatDetails();
