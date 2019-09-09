81ae2a210e19a76da38e5f68ae19d4b69251a08a
poplavskydn <poplavskydn@gmail.com>
2019-09-09 19:37:23 +0300

    payment workflow

--- paymentworkflow/src/test/java/com/kyripay/paymentworkflow/api/PaymentWorkflowApiTest.java
+++ paymentworkflow/src/test/java/com/kyripay/paymentworkflow/api/PaymentWorkflowApiTest.java
@@ -1,13 +1,24 @@
 package com.kyripay.paymentworkflow.api;
 
+import com.google.common.collect.ImmutableList;
 import com.kyripay.paymentworkflow.domain.dto.payment.Payment;
 import com.kyripay.paymentworkflow.stream.ConverterStreams;
 import com.kyripay.paymentworkflow.stream.PaymentStreams;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.json.JSONException;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.Customization;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.skyscreamer.jsonassert.RegularExpressionValueMatcher;
+import org.skyscreamer.jsonassert.comparator.CustomComparator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -26,9 +37,13 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.KafkaContainer;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
@@ -53,10 +68,30 @@ public class PaymentWorkflowApiTest {
     ConverterStreams converterStreams;
 
     @Test
-    public void incomingPaymentShouldBeSendToConverter() {
+    public void incomingPaymentShouldBeSendToConverter() throws JSONException {
         Payment payment = new Payment(1, UUID.randomUUID(), Payment.Status.CREATED, null, 111);
         kafkaTemplate.send("payment-workflow-process", 0, "2", "");
         // consumer from converter service
+        Properties consumerProperties = new Properties();
+        consumerProperties.put("bootstrap.servers", kafkaContainer.getBootstrapServers());
+        consumerProperties.put("group.id", "test");
+        consumerProperties.put("key.deserializer", StringDeserializer.class);
+        consumerProperties.put("value.deserializer", StringDeserializer.class);
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(consumerProperties);
+
+        consumer.subscribe(ImmutableList.of("converter-process"));
+
+        ConsumerRecords<String, String> messages = consumer.poll(Duration.ofMinutes(1));
+        assertEquals(1, messages.count());
+        String value = messages.records("converter-process").iterator().next().value();
+//        JSONAssert.assertEquals(
+//                "{\"documentId\":\"x\",\"status\":\"READY\"}",
+//                value,
+//                new CustomComparator(
+//                        JSONCompareMode.STRICT,
+//                        new Customization("documentId",  new RegularExpressionValueMatcher<>(".*"))
+//                )
+//        );
 
     }
 
